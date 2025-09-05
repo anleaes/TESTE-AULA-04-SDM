4d6df7be81672e4bfe7eafd71bf9544f659c0a32
Lulizza <luizafioravanters@gmail.com>
2025-09-05 10:33:31 -0300

    arrumando classe pessoa fisica

--- pessoa_fisica.py
+++ pessoa_fisica.py
@@ -1,2 +1,7 @@
-class PessoaFisica:
-    pass
+from pessoa import Pessoa
+
+class PessoaFisica(Pessoa):
+    def __init__(self, nome, endereco, idade, cpf, data_nascimento):
+        super().__init__(nome,  endereco, idade)
+        self._cpf = cpf
+        self._data_nascimento = data_nascimento
